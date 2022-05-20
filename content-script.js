fe3afa51709a0444b6d1434a592ca1c85040d2a0
ikuma-t <tadokorodev@gmail.com>
2022-05-20 20:09:49 +0900

    style: disabled tailwindcss
    TailwindCSS contaminates css global scope

--- content-script.js
+++ content-script.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './src/App.vue'
-import './src/index.css'
 
 const sideBar = document.querySelector(".navbar-side__nav")
 sideBar.insertAdjacentHTML('beforeend', '<div id="app"></div>')
