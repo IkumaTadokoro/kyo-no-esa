c50de72171ff375b25497adf30b25046d5375db5
ikuma-t <tadokorodev@gmail.com>
2022-05-20 14:21:52 +0900

    feat: add today's diary function
    If today's diary is exist: Open today's diary
    Else: Create new diary

--- App.vue
+++ App.vue
@@ -1,25 +1,62 @@
 <script setup lang="ts">
-// TODO: esaのAPIで日報情報を取得する
-// TODO: esaのAPIで日報情報が取得できなければ、日報を作成して、ページに遷移する。
+import { Configuration, EsaApi } from "./client"
+import { ref } from 'vue'
+
+const diaryUrl = ref({});
+
+const getToday = () => {
+  const date = new Date()
+  const year = date.getFullYear();
+  const month = ('00' + (date.getMonth() + 1)).slice(-2);
+  const day = ('00' + date.getDate()).slice(-2);
+
+  return (year + '/' + month + '/' + day);
+}
+
+(async () => {
+  const { accessToken, teamName, diaryCategory, templateId } = await chrome.storage.sync.get([
+    'accessToken',
+    'teamName',
+    'diaryCategory',
+    'templateId'
+  ])
+  const esaApi = new EsaApi(new Configuration({ accessToken }))
+  const q = `in:"${diaryCategory}" category:"${getToday()}"`
+  const { data } = await esaApi.getPosts({ teamName, q } )
+  const url = data.posts.length ? data.posts[0].url : `https://${teamName}.esa.io/posts/new?template_post_id=${templateId}`
+  diaryUrl.value = url
+})();
 </script>
 
 <template>
   <li class="navbar-side_item">
-    <a class="navbar-side_link" href="/posts">
-      今日のesa
+    <a class="navbar-side_link" :href="diaryUrl">
+      今日の<br>esa
     </a>
   </li>
 </template>
 
 <style scoped>
 li {
-  background-color: #42b983;
+  display: list-item;
+  border-bottom: 1px solid rgba(0,0,0,0.05);
   width: 80px;
-  height: 80px;
+  height: 68px;
 }
 
 a {
+  display: block;
+  text-align: center;
+  width: 80px;
+  height: 68px;
+  padding: 8px 0 8px 0;
+  font-size: 16px;
   text-decoration: none;
+  color: rgba(0,0,0,0.35);
+}
+
+a:hover {
   color: white;
+  text-decoration: none;
 }
 </style>
