5d7a3a599c52bb71c955478cc605feaf657fe1b0
ikuma-t <tadokorodev@gmail.com>
2022-05-20 17:08:05 +0900

    feat: add tailwindcss

--- content-script.js
+++ content-script.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './src/App.vue'
+import './src/index.css'
 
 const sideBar = document.querySelector(".navbar-side__nav")
 sideBar.insertAdjacentHTML('beforeend', '<div id="app"></div>')
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "options.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
