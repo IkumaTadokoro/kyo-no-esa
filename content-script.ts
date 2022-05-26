dcc15ae98358d70bd53bd22e39e8001cfacc0ff9
ikuma-t <tadokorodev@gmail.com>
2022-05-26 11:48:51 +0900

    refactor: Change file type of content-script

--- content-script.ts
+++ content-script.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './src/App.vue'
 
-const sideBar = document.querySelector(".navbar-side__nav")
+const sideBar = document.querySelector(".navbar-side__nav")!
 sideBar.insertAdjacentHTML('beforeend', '<div id="app"></div>')
 
 const selector = '#app'
